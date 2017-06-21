5e6712aed0046c2e4288b82cceba25f884ffa010
CharlesP17 <charles.paradot@viacesi.fr>
2017-06-21 09:37:16 +0200

    FDP

---
+++ boulderdash/model/src/main/java/model/ModelFacade.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.dao.ExampleDAO;
+
+/**
+ * <h1>The Class ModelFacade provides a facade of the Model component.</h1>
+ *
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @version 1.0
+ */
+public class ModelFacade implements IModel {
+
+    /**
+     * Instantiates a new model facade.
+     */
+    public ModelFacade() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see model.IModel#getExampleById(int)
+     */
+    public Example getExampleById(final int id) throws SQLException {
+        return ExampleDAO.getExampleById(id);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see model.IModel#getExampleByName(java.lang.String)
+     */
+    public Example getExampleByName(final String name) throws SQLException {
+        return ExampleDAO.getExampleByName(name);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see model.IModel#getAllExamples()
+     */
+    public List<Example> getAllExamples() throws SQLException {
+        return ExampleDAO.getAllExamples();
+    }
+    public ArrayList<Elements> IDElements;
+}
