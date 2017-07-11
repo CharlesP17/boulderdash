a7612746862eb36bf0088b01172ff6d25007f0d2
CharlesP17 <charles.paradot@viacesi.fr>
2017-07-11 11:19:38 +0200

    FIN

--- boulderdash/model/src/main/java/model/dao/Connect.java
+++ boulderdash/model/src/main/java/model/dao/Connect.java
@@ -34,7 +34,7 @@ try
 {
 	cn = DriverManager.getConnection(url, login, passwd);
 	st = (Statement) cn.createStatement();
-	String sql = "call GetMap(2)";
+	String sql = "call GetMap(4)";
 	rs = st.executeQuery(sql);
 	while (rs.next()){
 		wordmap=rs.getString("Map");}
